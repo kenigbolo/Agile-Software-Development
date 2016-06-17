f414fd60967bbbbda81c70c11d32703ba1817a8d
kenigbolo <kenigbol@ut.ee>
2016-06-17 16:43:18 +0300

    Finnished with first homework

--- hw-ruby-intro-master/lib/ruby_intro.rb
+++ hw-ruby-intro-master/lib/ruby_intro.rb
@@ -1,10 +1,9 @@
 # When done, submit this entire file to the autograder.
 
 # Part 1
-require "pry"
+require "pry" #For debbuging purposes
 
 def sum (arr)
-  # YOUR CODE HERE
   sum = 0
   arr.each do |add|
   	sum = sum + add
@@ -13,7 +12,6 @@ def sum (arr)
 end
 
 def max_2_sum (arr)
-  # YOUR CODE HERE
   empty = 0
   arr = arr.sort
   if arr == []
@@ -26,27 +24,60 @@ def max_2_sum (arr)
   end
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n? (arr, n)
+  if arr.length == 0 || arr.length == 1
+  	return false
+  else
+  	arr.combination(2).any? {|a, b| a + b == n }
+  end  
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  name = name.to_s
+  hello = "Hello, " + name
+  return hello
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant? (s)
+	if s[0].class.name == "String"
+		s[0] = s[0].downcase 
+		if ( /[a-z]/ =~ s[0] && /[^aeiou]/ =~ s[0])
+			return true
+		else
+			return false
+		end
+	elsif s.empty?
+		return false
+	else
+		return false
+	end
+  
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4? (s)
+	if (s =~ /^[0-1]+$/) && (s.to_i % 4 == 0)
+		return true
+	else
+		return false
+	end
 end
 
 # Part 3
 
-class BookInStock
+class BookInStock 
 # YOUR CODE HERE
+	attr_accessor :isbn, :price
+	def initialize (isbn, price)
+		raise ArgumentError if isbn.empty? || price <= 0
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string
+		"$%.2f" % @price
+	end
+
 end
 
